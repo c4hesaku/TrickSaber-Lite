1a228e089e463b2e67808a7f8650243dee2b3657
laynia <vc_mikuu@hotmail.com>
2025-06-23 23:18:41 +0200

    Updating scripts

--- scripts/validate-modjson.ps1
+++ scripts/validate-modjson.ps1
@@ -1,31 +1,51 @@
-$mod = "./mod.json"
+# This script validates the mod.json file against a schema.
+# It updates mod.json if mod.template.json or qpm.shared.json are newer.
+# If PowerShell version is 6 or higher, it validates mod.json using a schema.
 
-if (-not (Test-Path -Path $mod)) {
-    if (Test-Path -Path ".\mod.template.json") {
-        & qpm-rust qmod build
+$mod = "./mod.json"
+$modTemplate = "./mod.template.json"
+$qpmShared = "./qpm.shared.json"
+
+# Check if mod.template.json exists
+if (Test-Path -Path $modTemplate) {
+    # Determine if mod.json needs to be updated
+    $update = -not (Test-Path -Path $mod)
+
+    if (-not $update) {
+        $update = (Get-Item $modTemplate).LastWriteTime -gt (Get-Item $mod).LastWriteTime
+    }
+
+    if (-not $update -and (Test-Path -Path $qpmShared)) {
+        $update = (Get-Item $qpmShared).LastWriteTime -gt (Get-Item $mod).LastWriteTime
+    }
+
+    # Update mod.json if necessary
+    if ($update) {
+        & qpm qmod manifest
         if ($LASTEXITCODE -ne 0) {
             exit $LASTEXITCODE
         }
     }
-    else {
-        Write-Output "Error: mod.json and mod.template.json were not present"
-        exit 1
-    }
+}
+# Error if neither mod.json nor mod.template.json exist
+elseif (-not (Test-Path -Path $mod)) {
+    Write-Output "Error: mod.json and mod.template.json were not present"
+    exit 1
 }
 
 Write-Output "Creating qmod from mod.json"
 
+# Validate mod.json if PowerShell version is 6 or higher
 $psVersion = $PSVersionTable.PSVersion.Major
 if ($psVersion -ge 6) {
+    # Download the schema
     $schemaUrl = "https://raw.githubusercontent.com/Lauriethefish/QuestPatcher.QMod/main/QuestPatcher.QMod/Resources/qmod.schema.json"
-    Invoke-WebRequest $schemaUrl -OutFile ./mod.schema.json
+    $modSchemaRaw = (Invoke-WebRequest $schemaUrl).Content
 
-    $schema = "./mod.schema.json"
+    # Read mod.json
     $modJsonRaw = Get-Content $mod -Raw
-    $modSchemaRaw = Get-Content $schema -Raw
 
-    Remove-Item $schema
-
+    # Validate mod.json
     Write-Output "Validating mod.json..."
     if (-not ($modJsonRaw | Test-Json -Schema $modSchemaRaw)) {
         Write-Output "Error: mod.json is not valid"
